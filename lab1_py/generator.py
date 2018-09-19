375e073be4c847c9b23f90001086e90b5e0de1c2
arlovaalina <arlovaalina@gmail.com>
2018-09-19 18:10:37 +0300

    Add version on python

---
+++ lab1_py/generator.py
@@ -0,0 +1,42 @@
+from functools import reduce
+import statistics as stat
+import math
+
+
+def generate_numbers(a, r, m, numbersAmount):
+    numbers = []
+    for i in range(numbersAmount):
+        rNew = (a * r) % m
+        x = rNew / m
+        r = rNew 
+        numbers.append(x)
+    return numbers
+
+    
+def find_expectation(numbers):
+    # sum = reduce((lambda a, b: a + b), numbers)
+    # return sum / len(numbers)
+    return stat.mean(numbers)
+
+    
+def find_dispersion(numbers, m):
+    # sum = reduce((lambda a, b: a + (b - m)*(b - m)), numbers)
+    # return sum / len(numbers)
+    return stat.variance(numbers)
+
+    
+def find_standard_deviation(dispersion):
+    return math.sqrt(dispersion)
+    
+
+def find_period(numbers):
+    compareValue = numbers[len(numbers) - 1]
+    firstIndex = numbers.index(compareValue)
+    secondIndex = numbers.index(compareValue, firstIndex + 1)
+    return secondIndex - firstIndex
+    
+    
+def find_aperiodic_interval(numbers, period):
+    for i in range(len(numbers)):
+        if numbers[i] == numbers[i + period]:
+            return i + period
---
+++ lab1_py/main.py
@@ -0,0 +1,88 @@
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+from generator import *
+from tkinter import *
+from variables import *
+
+
+def show_chart(root, numbers):
+    minValue = min(numbers)
+    maxValue = max(numbers)
+    
+    numbersRange = maxValue - minValue
+    barLength = numbersRange / BARS_IN_CHART
+    
+    frequencies = []
+    intervals = []
+    minBoundary = minValue
+    maxBoundary = minBoundary + barLength
+    
+    for i in range(BARS_IN_CHART):
+        numbersInInterval = list(filter(lambda number: number >= minBoundary and number < maxBoundary, numbers))
+        frequency = len(numbersInInterval) / len(numbers)
+        frequencies.append(frequency)
+        intervals.append(minBoundary)
+        minBoundary += barLength
+        maxBoundary += barLength
+    
+    figure = Figure(figsize = (6, 4), dpi = 100)
+    a = figure.add_subplot(111)
+    a.bar(intervals, frequencies, barLength)
+    canvas = FigureCanvasTkAgg(figure, root)
+    canvas.show()
+    canvas.get_tk_widget().place(x = 200, y = 10)
+
+
+def start_algorithm(root, a, r, m, numbersAmount):
+    numbers = generate_numbers(a, r, m, numbersAmount)
+    print(numbers)
+    
+    expectation = find_expectation(numbers)
+    dispersion = find_dispersion(numbers, expectation)
+    standard_deviation = find_standard_deviation(dispersion)
+    period = find_period(numbers)
+    aperiodic_interval = find_aperiodic_interval(numbers, period)
+    
+    show_chart(root, numbers)
+    
+    expectation_label = Label(root, text = "Мат. Ожидание: " + str(round(expectation, 3))).place(x = 200, y = 420)
+    dispersion_label = Label(root, text = "Дисперсия: " + str(round(dispersion, 3))).place(x = 200, y = 440)
+    standard_deviation_label = Label(root, text = "Среднеквадратическое отклонение: " + str(round(standard_deviation, 3))).place(x = 200, y = 460)
+    period_label = Label(root, text = "Период: " + str(round(period, 3))).place(x = 200, y = 480)
+    aperiodic_interval_label = Label(root, text = "Длина отрезка апериодичности: " + str(round(aperiodic_interval, 3))).place(x = 200, y = 500)
+
+def initialize_window():
+    root = Tk()
+    root.geometry("{0}x{1}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    a_value_label = Label(root, text = "Параметр а").place(x = 10, y = 10)
+    a_value_input = Entry(root)
+    a_value_input.place(x = 10, y = 30)
+    
+    r_value_label = Label(root, text = "Параметр r").place(x = 10, y = 60)
+    r_value_input = Entry(root)
+    r_value_input.place(x = 10, y = 80)
+    
+    m_value_label = Label(root, text = "Параметр m").place(x = 10, y = 110)
+    m_value_input = Entry(root)
+    m_value_input.place(x = 10, y = 130)
+    
+    n_value_label = Label(root, text = "Количество чисел").place(x = 10, y = 160)
+    n_value_input = Entry(root)
+    n_value_input.place(x = 10, y = 180)
+    
+    start_button = Button(root, text = "Начать", width = 10,
+        command = lambda: start_algorithm(root, int(a_value_input.get()), int(r_value_input.get()), int(m_value_input.get()), int(n_value_input.get())))
+    start_button.place(x = 10, y = 210)
+
+    root.mainloop()
+
+
+def main():
+    initialize_window();
+
+if __name__ == "__main__":
+    main()
